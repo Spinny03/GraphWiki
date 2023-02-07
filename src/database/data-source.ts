22410a86ba131afb8d79207a3d5c94880c19ce2f
SpinnyIkinova <80131347+Spinny03@users.noreply.github.com>
2023-02-07 22:15:04 +0100

    riuscito con username, aggiunto article

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import {DataSource} from 'typeorm';
 import {User} from './entity/User';
+import {Article} from './entity/Article';
 
 export const AppDataSource = new DataSource({
     useUnifiedTopology: true,
@@ -8,7 +9,7 @@ export const AppDataSource = new DataSource({
     database: 'database',
     synchronize: true,
     logging: false,
-    entities: [User],
+    entities: [User, Article],
     migrations: [],
     subscribers: [],
 });
